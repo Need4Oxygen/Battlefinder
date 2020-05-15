7aa9879cbd4db72643ba5a2ab729999b14bb0486
Manuel Segura <manuillo94@gmail.com>
2020-05-15 18:29:38 +0200

    Patron Attribution

--- Assets/Scripts/Main Menu/ExternalLinks.cs
+++ Assets/Scripts/Main Menu/ExternalLinks.cs
@@ -1,4 +1,7 @@
-﻿using UnityEngine;
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
 
 public class ExternalLinks : MonoBehaviour
 {
@@ -9,4 +12,26 @@ public class ExternalLinks : MonoBehaviour
         else
             Debug.LogWarning("[ExternalLinks] Trying to open empty URL!");
     }
+
+
+    // public string accessToken;
+
+    // public void Start()
+    // {
+    //     StartCoroutine(GetDataAsync());
+    // }
+
+    // private IEnumerator GetDataAsync()
+    // {
+    //     Dictionary<string, string> headers = new Dictionary<string, string>();
+    //     headers.Add("authorization", "Bearer " + accessToken);
+    //     DownloadHandlerBuffer down = new DownloadHandlerBuffer();
+    //     UnityWebRequest patreonRequest = new UnityWebRequest("https://www.patreon.com/api/oauth2/api/current_user", "HTTP GET", down, null);
+
+    //     yield return patreonRequest.SendWebRequest();
+
+    //     Debug.Log(patreonRequest.responseCode);
+    //     Debug.Log(down.text);
+    // }
+
 }
