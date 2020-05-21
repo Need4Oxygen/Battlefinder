a2a8743ba4b4790340cad62831122ceb60b53e62
Manuel Segura <manuillo94@gmail.com>
2020-05-21 15:27:26 +0200

    Positionables

--- Assets/Scripts/PF2e/Data Containers/Campaings/PF2E_BoardData.cs
+++ Assets/Scripts/PF2e/Data Containers/Campaings/PF2E_BoardData.cs
@@ -1,9 +1,11 @@
-using UnityEngine;
+using System.Collections.Generic;
 
 public class PF2E_BoardData
 {
+    public string guid = "";
+    public string boardName = "";
 
-    public string guid;
-
-
+    public List<PositionableActor> players = new List<PositionableActor>();
+    public List<PositionableActor> enemies = new List<PositionableActor>();
+    public List<PositionableActor> npcs = new List<PositionableActor>();
 }
--- Assets/Scripts/Statics/Enums.cs
+++ Assets/Scripts/Statics/Enums.cs
@@ -7,3 +7,7 @@ public enum E_Languages { None, Default, English, Spanish }
 public enum E_Tools { None, Walls, Floors }
 
 public enum E_Game { None, Default, PF2E }
+
+public enum E_Actors { None, Default, Player, Enemy, NPC }
+
+public enum E_Positionables { None, Default, Actor, }
