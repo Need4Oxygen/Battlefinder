10be13497208c4d2262118e0a8db248af4037f0c
Manuel Segura <manuillo94@gmail.com>
2020-05-30 14:19:12 +0200

    Reorder folders + small edit UI Blocker

---
+++ Assets/Scripts/UI/Behaviours/MouseInputUIBlocker.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+// Original script by: Sjonsson (Unity Forums: https://answers.unity.com/questions/967170/detect-if-pointer-is-over-any-ui-element.html)
+// Modified by: Manuel Segura
+
+[RequireComponent(typeof(EventTrigger))]
+public class MouseInputUIBlocker : MonoBehaviour
+{
+    public static bool BlockedByUI;
+    private EventTrigger eventTrigger;
+
+    private void Start()
+    {
+        eventTrigger = GetComponent<EventTrigger>();
+
+        EventTrigger.Entry enterUIEntry = new EventTrigger.Entry();
+        enterUIEntry.eventID = EventTriggerType.PointerEnter;
+        enterUIEntry.callback.AddListener((eventData) => { SetBlocked(true); });
+        eventTrigger.triggers.Add(enterUIEntry);
+
+        EventTrigger.Entry exitUIEntry = new EventTrigger.Entry();
+        exitUIEntry.eventID = EventTriggerType.PointerExit;
+        exitUIEntry.callback.AddListener((eventData) => { SetBlocked(false); });
+        eventTrigger.triggers.Add(exitUIEntry);
+    }
+
+    private void SetBlocked(bool value)
+    {
+        BlockedByUI = value;
+    }
+}
