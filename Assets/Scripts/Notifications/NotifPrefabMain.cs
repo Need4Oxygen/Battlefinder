f56656e7fbe1164c066229246e1147beb269e47c
Sergio <sergioarroyoap@gmail.com>
2020-06-03 15:32:06 +0200

    Notification system

---
+++ Assets/Scripts/Notifications/NotifPrefabMain.cs
@@ -0,0 +1,89 @@
+﻿using System.Collections;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class NotifPrefabMain : MonoBehaviour
+{
+    [HideInInspector]
+    public NotificationManager notificationManager;
+    public CanvasGroup fadeCanvasGroup;
+    public Image background;
+    public Image image;
+    public TMP_Text text;
+    public float speed;
+    public float lifespan;
+    public float fadeDuration;
+
+    RectTransform self;
+    int positionInQueue = 0;
+    float height;
+
+    public NotifPrefabMain(Image _image, TMP_Text _text)
+    {
+        image = _image;
+        text = _text;
+    }
+
+    //suscribo esto al NotificationManager para saber cuando sale otra notificacion
+    //se suscribe al instanciarlo en Notification Manager
+    public void Rearange(int i)
+    {
+        positionInQueue = i;
+    }
+
+    public void Start()
+    {
+        self = GetComponent<RectTransform>();
+        height = self.sizeDelta.y;
+
+        //posiciono el objeto por encima de la pantalla rozando
+        self.anchoredPosition = new Vector2(0, height);
+        self.localPosition = new Vector3(self.localPosition.x, self.localPosition.y, 0);//pongo la z a cero
+
+        //comienzo el fadeout
+        StartCoroutine(FadeCanvasGroup(fadeCanvasGroup, 0f, fadeDuration));
+    }
+
+    void Update()
+    {
+        Vector2 targetPos = new Vector2(0, -positionInQueue * height);
+        Vector2 distance = targetPos - self.anchoredPosition;
+
+        //me mueve el objeto
+        Vector2 newPos = self.anchoredPosition + distance * speed * Time.unscaledDeltaTime;
+        self.anchoredPosition = newPos;
+    }
+
+    IEnumerator FadeCanvasGroup(CanvasGroup cg, float alphaTarget, float duration)
+    {
+        yield return new WaitForSecondsRealtime(0.1f);
+        yield return new WaitForSecondsRealtime(lifespan);
+
+        float counter = 0;
+        float completion = 0;
+        float alphaInit = cg.alpha;
+        float alphaDif = alphaInit - alphaTarget;
+
+        if (alphaDif != 0)
+            while (counter < duration)
+            {
+                counter += Time.unscaledDeltaTime;
+                completion = counter / duration;
+
+                cg.alpha = -Mathf.Clamp(completion, 0f, 1f) * alphaDif + alphaInit;
+                yield return new WaitForSecondsRealtime(0.01f);
+            }
+        else
+            Debug.Log("Fade failed: initial alpha equal to target");
+
+        cg.alpha = alphaTarget;
+
+        //le digo al manager que me he muerto
+        if (notificationManager != null)
+            notificationManager.NotifDeath(gameObject);
+
+        Destroy(gameObject);
+    }
+
+}
---
+++ Assets/Scripts/Notifications/NotificationManager.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public enum NotifSprites
+{
+    _default, _deco1, _deco2, _deco3, _deco4, _deco5, _exclamation,
+    _tick, _cross, _arrow
+};
+public enum NotifColors { _default, _green, _orange, _red, _blue };
+
+public class NotificationManager : MonoBehaviour
+{
+    //singleton
+    public static NotificationManager Instance { get; private set; }
+
+    [Serializable]
+    public struct PicAndName
+    {
+        public NotifSprites spriteName;
+        public Sprite sprite;
+    }
+
+    [Serializable]
+    public struct ColorAndName
+    {
+        public NotifColors notifColor;
+        public Color color;
+    }
+
+    [SerializeField] GameObject messageStandarPrefab = default;
+    [SerializeField] RectTransform canvas = default;
+
+    public List<PicAndName> spriteList = new List<PicAndName>();
+    public List<ColorAndName> colorList = new List<ColorAndName>();
+    public List<GameObject> activeNotifications = new List<GameObject>();
+    Dictionary<NotifSprites, Sprite> spriteDic = new Dictionary<NotifSprites, Sprite>();
+    Dictionary<NotifColors, Color> colorDic = new Dictionary<NotifColors, Color>();
+
+    //singleton stuff
+    void Awake()
+    {
+        if (Instance == null)
+        {
+            DontDestroyOnLoad(transform.parent);
+            Instance = this;
+        }
+        else
+            Destroy(transform.parent.gameObject);
+    }
+
+    void Start()
+    {
+        foreach (PicAndName item in spriteList)
+            spriteDic.Add(item.spriteName, item.sprite);
+        foreach (ColorAndName item in colorList)
+            colorDic.Add(item.notifColor, item.color);
+    }
+
+    public void Notify(NotifColors colorName, NotifSprites spriteName, string text, float lifespan, float delay)
+    {
+        if (delay > 0)
+            StartCoroutine(NotifyWithDelay(colorName, spriteName, text, lifespan, delay));
+        else
+            Notify(colorName, spriteName, text, lifespan);
+    }
+    IEnumerator NotifyWithDelay(NotifColors colorName, NotifSprites spriteName, string text, float lifespan, float delay)
+    {
+        yield return new WaitForSecondsRealtime(delay);
+        Notify(colorName, spriteName, text, lifespan);
+    }
+    public void Notify(NotifColors colorName, NotifSprites spriteName, string text, float lifespan)
+    {
+        //spawneo la notificacion
+        GameObject newNotif = Instantiate(messageStandarPrefab, transform.position, Quaternion.identity, canvas.transform);
+        NotifPrefabMain notifScript = newNotif.GetComponent<NotifPrefabMain>();
+
+        //inserto la nueva notif como la primera
+        activeNotifications.Insert(0, newNotif);
+
+        //le meto el lifespan
+        notifScript.lifespan = lifespan;
+
+        // sprite
+        if (spriteDic.ContainsKey(spriteName))
+            notifScript.image.sprite = spriteDic[spriteName];
+        else
+            notifScript.image.sprite = spriteDic[NotifSprites._default];
+
+        // color
+        if (colorDic.ContainsKey(colorName))
+            notifScript.background.color = colorDic[colorName];
+        else
+            notifScript.background.color = colorDic[NotifColors._default];
+
+        // texto
+        if (text != null && text != "")
+            notifScript.text.text = text;
+        else
+            notifScript.text.text = "default_text_error_lol";
+
+        //meto la referencia de este script
+        notifScript.notificationManager = this;
+
+        CallRearange();
+    }
+
+    public void NotifDeath(GameObject notification)
+    {
+        //paso por la lista de notificaciones activas y las rearrangeo
+        if (activeNotifications.Contains(notification))
+            activeNotifications.Remove(notification);
+
+        CallRearange();
+    }
+
+    void CallRearange()
+    {
+        for (int i = 0; i < activeNotifications.Count; i++)
+        {
+            activeNotifications[i].GetComponent<NotifPrefabMain>().Rearange(i);
+        }
+    }
+
+}
