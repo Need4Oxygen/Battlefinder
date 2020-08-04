d68d7039b350ba0d4d7557d18c1c598a74960050
Manuel Segura <manuillo94@gmail.com>
2020-08-04 17:29:06 +0200

    Metas

---
+++ Assets/Scripts/EditorTools.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class EditorTools : MonoBehaviour
+{
+
+    [MenuItem("Tools/ConvertBackgrounds")]
+    void ConvertBackgrounds()
+    {
+
+        // string backgrounds =
+
+    }
+
+}
