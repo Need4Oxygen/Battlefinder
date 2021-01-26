a333a1f006b47017e55eef67b100201f134ba2a3
Manuel Segura <manuillo94@gmail.com>
2021-01-26 12:17:16 +0100

    Window Raycast Focus system

--- Assets/Scripts/UI/Windows/Window.cs
+++ Assets/Scripts/UI/Windows/Window.cs
@@ -1,14 +1,24 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Window : MonoBehaviour
 {
-    public Canvas canvas = null;
-    public CanvasGroup group = null;
+    public string windowName = "";
 
     [Space(15)]
-    public bool setClosedOnAwake = true;
+    public Canvas canvas = null;
+    public CanvasGroup group = null;
+    public GraphicRaycaster raycaster = null;
+
+    [Space(15)]
+    public bool startClosed = true;
+    public bool raycastTarget = true;             // isolate raycast on self and children when opened
+
+    [Space(15)]
+    public bool isSubpanel = false;               // don't go back in raycast stack
+    public bool syncRaycastWithParent = false;    // when parent raycast activates, this window's raycast will activate too
 
     [Space(15)]
     public float openScaleTarget = 1f;
@@ -21,23 +31,35 @@ public class Window : MonoBehaviour
     [Space(15)]
     public float openDelay = 0f;
     public float closeDelay = 0f;
-    public float closeDelayIfLast = 0.1f; // delay if this is the last opened window in the hierarchy, so camera have time to turn back on
+    public float closeDelayIfLast = 0.1f;          // delay if this is the last opened window in the hierarchy, so camera have time to turn back on
     public float openCloseAnimDuration = 0.1f;
+
+    [Space(15)]
+    public List<Window> children = new List<Window>();
 
     void Awake()
     {
-        if (setClosedOnAwake)
+        if (raycaster != null)
+            raycaster.enabled = false;
+
+        if (startClosed)
             StartCoroutine(PanelFader.RescaleAndFadeWindow(this, closeScaleTarget, closeAlphaTarget, 0f, 0f));
     }
 
     public void OpenWindow()
     {
+        if (raycaster != null)
+            raycaster.enabled = true;
+
         WindowManager.WindowOpened(this);
         StartCoroutine(PanelFader.RescaleAndFadeWindow(this, openScaleTarget, openAlphaTarget, openDelay, openCloseAnimDuration));
     }
 
     public void CloseWindow()
     {
+        if (raycaster != null)
+            raycaster.enabled = false;
+
         WindowManager.WindowClosed(this);
         StartCoroutine(PanelFader.RescaleAndFadeWindow(this, closeScaleTarget, closeAlphaTarget, WindowManager.OpenWindows.Count > 0 ? closeDelay : closeDelayIfLast, openCloseAnimDuration));
     }
